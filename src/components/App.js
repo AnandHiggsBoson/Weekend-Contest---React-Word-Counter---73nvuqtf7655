379ca751d04b6d40b683ba29dfca9521d3389ba6
Anand Darshan <101782811+AnandHiggsBoson@users.noreply.github.com>
2022-07-02 23:49:41 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import '../styles/App.css';
-const App = () => {
+import { useState } from "react";
 
+const App=()=>{
+  const [charLimit, setCharLimit] = useState(50)
+  const[fontSize, setFontSize] = useState(16)
+  const[charCount , setCharCount ] = useState(0)
+  const[wordCount, setWordCount] = useState(0)
+ 
+ const onChangeHandler =(e) =>{
+    console.log(e.target.value);
+    const text = e.target.value;
 
-  return (
-    <div id="main">
+   setCharCount(text.length);
+  
+   let w_count = 0;
+   if(text.length>0)
+   w_count= text.trim().split(" ").length;
+    setWordCount(w_count);
+ };
+
+    const onCharLimitChangeHandler = (e) =>{
+      setCharLimit(parseInt(e.target.value));
+    };
+    
+   
+  
+  return(
+    <div>
+      <input type="range" id="fontSize-input" min ="16" max = "32"  onChange={(e) =>{
+        setFontSize(e.target.value);
+      }}/> 
+      <br /><br />
+      <input type="number" id="char-limit-input" value={charLimit} onChange={onCharLimitChangeHandler} /> 
+      <br /> <br />
+      <textarea style= {{fontSize: fontSize+ 'px'}} onChange= {onChangeHandler} maxLength={charLimit}></textarea>
+      <br /> <br />
+      <div id ="word-counter"> Total number of words written: {wordCount}</div>
+      <br /> <br />
+      <div id = "char-counter">Total number of characters used: {charCount} </div>
+      <br /> <br />
     </div>
   )
 }
-
-
 export default App;
